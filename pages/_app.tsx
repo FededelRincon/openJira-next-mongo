958fc2dbb9aaebc9eaad0fb511e1cb6120343e09
FDR <delrincon@gmail.com>
2022-03-21 12:02:20 -0300

    first commit + mui set it up + 2 themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline';
+import { lightTheme, darkTheme } from '../themes';
+
+
+
+
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={ darkTheme }>
+        {/* <ThemeProvider theme={ lightTheme }> */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
---
+++ themes/dark-theme.ts
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        // background: {
+        //     default: grey[400],
+        // },
+        // primary: {
+        //     main: '#4a148c'
+        // },
+        secondary: {
+            main: '#19857b'
+        },
+        error: {
+            main: red[400]
+        },
+
+    },
+    components: {
+        
+    }
+});
