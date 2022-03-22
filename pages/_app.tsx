70322567ec0728f9432088b7bca40d84eee5573e
FDR <delrincon@gmail.com>
2022-03-21 22:27:39 -0300

    UIContext done and its conected to Sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline';
+
 import { lightTheme, darkTheme } from '../themes';
-
-
-
-
+import { UIProvider } from '../context/ui';
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider theme={ darkTheme }>
-        {/* <ThemeProvider theme={ lightTheme }> */}
-            <CssBaseline />
-            <Component {...pageProps} />
-          
-        </ThemeProvider>
+        <UIProvider>
+            <ThemeProvider theme={ darkTheme }>
+            {/* <ThemeProvider theme={ lightTheme }> */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </UIProvider>
     )
 }
 
